fcc604c2c49085649eea11c16fc7fc638bbf94f4
CharbelMt <ccmattar2005@gmail.com>
2026-03-24 22:55:50 +0200

    translation file

--- desktop/src/i18n/en-US/index.ts
+++ desktop/src/i18n/en-US/index.ts
@@ -2,6 +2,14 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  login: {
+    enter_email: 'Enter your email',
+    required: 'This field is required',
+    enter_password: 'Enter your password',
+    invalid_email: 'Email is invalid',
+    invalid_password: 'Password is invalid',
+    credentials: 'Enter your credentials',
+    remember_me: 'Remember me',
+    login: 'Login',
+  },
 };
